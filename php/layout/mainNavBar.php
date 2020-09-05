e22669cbe4afca5090b000ebbf5dbf92cdce9892
Guillaume Quint <g.quint@studenti.unipi.it>
2020-09-05 15:54:05 +0200

    fix change notif-container to div

--- php/layout/mainNavBar.php
+++ php/layout/mainNavBar.php
@@ -1,4 +1,4 @@
-<nav id="main-nav"><span id="notif-container"><div id="notif-unread"></div></span>
+<nav id="main-nav"><div id="notif-container"><div id="notif-unread"></div></div>
     <input type="radio" name="nav-btn" id="messages"> </input><label for="messages" onclick="loadMessages(<?php echo $_SESSION['user_id'] . ', ' . (isset($_SESSION['ristorante'])?$_SESSION['ristorante']:'-1') ?>)" class="material-icons box glow-fuchsia">mail</label>
     <input type="radio" name="nav-btn" id="home"> </input><label for="home" onclick="loadMainHome(<?php echo $_SESSION['user_id']?>)" class="material-icons box">home</label>
     <input type="radio" name="nav-btn" id="preferences"> </input><label for="preferences" onclick="loadPreferences(<?php echo $_SESSION['user_id'] ?>)" class="material-icons box glow-orange">settings</label>
